8ec31788f45b86586424d818cf29f4c8a014287d
Tomasz Janczuk <tomasz@janczuk.org>
2015-03-05 11:48:28 -0800

    fix typo

--- github/webhook.js
+++ github/webhook.js
@@ -1,3 +1,5 @@
+// Raw version of this file: http://bit.ly/1A1wkC3
+
 var async = require('async');
 
 return function (context, req, res) {
@@ -6,7 +8,7 @@ return function (context, req, res) {
     async.series([
         function (callback) {
             // Collect payload of GitHub web hook
-            req.on('data', function (chunk) { body += data; });
+            req.on('data', function (chunk) { body += chunk; });
             req.on('end', function () {
                 try {
                     body = JSON.parse(body);
