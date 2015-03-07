e781f76ee34d0e4e7c4ceef73953f63ceed1c642
Tomasz Janczuk <tomasz@janczuk.org>
2015-03-06 17:52:54 -0800

    update

--- login/bendy_banana.js
+++ login/bendy_banana.js
@@ -1,5 +1,5 @@
 // Webtask URL:
-// https://sandbox.it.auth0.com/auth0-wf?webtask_no_cache=1&eyJhbGciOiJIUzI1NiIsImtpZCI6IjEifQ.eyJqdGkiOiI5YjFjMGNlNjgyZTQ0NzlhODY1ZTE2Y2JlYjdjNzVmZiIsImlhdCI6MTQyNTY3Nzc4MywidXJsIjoiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2F1dGgwL3dlYnRhc2stc2NyaXB0cy9tYXN0ZXIvbG9naW4vYXV0aDBfbG9naW4uanMiLCJ0ZW4iOiJhdXRoMC13ZiIsInBjdHgiOnsiYXV0aDBfZG9tYWluIjoiYXV0aDAtd2YuYXV0aDAuY29tIiwiYXV0aDBfY2xpZW50X2lkIjoiZ2RVOG50NVFiZE1BcGM4N2VkN2UxQlRlWTQ2TzdDemMifX0.MvwWuM__Y_z8WRUL4QnSA9foxL__eq2l6Q3ywcwzkmw
+// https://sandbox.it.auth0.com/auth0-wf?webtask_no_cache=1&key=eyJhbGciOiJIUzI1NiIsImtpZCI6IjEifQ.eyJqdGkiOiI4MmY1YzEyNjA3YTE0YTEyYTUzNjFmMGFhZDliNGE0ZiIsImlhdCI6MTQyNTY5MjcxNCwidXJsIjoiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2F1dGgwL3dlYnRhc2stc2NyaXB0cy9tYXN0ZXIvbG9naW4vYmVuZHlfYmFuYW5hLmpzIiwidGVuIjoiYXV0aDAtd2YiLCJwY3R4Ijp7ImF1dGgwX2RvbWFpbiI6ImF1dGgwLXdmLmF1dGgwLmNvbSIsImF1dGgwX2NsaWVudF9pZCI6ImdkVThudDVRYmRNQXBjODdlZDdlMUJUZVk0Nk83Q3pjIn19.bC9VOhFgCGOVBwOzX47nyA3deIV9DrQUqJC4Fvh1Www
 
 var async = require('async')
     , querystring = require('querystring');
@@ -59,44 +59,20 @@ function view() {/*
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>Login</title>
+    <title>Bendy banana</title>
     <style>
         body { 
             background: #44C7F4;
         }
     </style>
-    <script src="//cdn.auth0.com/js/lock-7.1.min.js"></script>
 <head>
 <body>
-    <h1>Unnecessary 3s delay to show we are in webtask...</h1>
-    <script>
-        var lock;
-        document.addEventListener('DOMContentLoaded', function() {
-            lock = new Auth0Lock('<%- data.auth0_client_id %>', '<%- data.auth0_domain %>');
-            setTimeout(function () {
-                lock.show({ 
-                    popup: true,
-                    connections: ['<%- data.strategy %>']
-                    <% if (data.title) { %>
-                    , dict: {
-                        signin: {
-                            title: '<%= data.title %>'
-                        }
-                    }
-                    <% }; %>
-                }, function(error, profile, token) {
-                    var hash = '#state=<%= data.state %>&';
-                    if (error) {
-                        hash += 'error=' + encodeURIComponent(error.message || error.toString());
-                    }
-                    else {
-                        hash += 'token=' + encodeURIComponent(token);
-                    }
-                    window.location.replace('<%- data.callback %>' + hash);
-                });
-            }, 3000);
-        });
-    </script>
+    <h1>Bendy banana law</h1>
+    <p>EU laws require us to ask for your acceptance of the provisions of 
+    <a href="http://en.wikipedia.org/wiki/Commission_Regulation_%28EC%29_No_2257/94" target="_system">Bendy Banana Law</a> 
+    before we can proceed.</p>
+    <a href="<%- data.callback %>#state=<%- data.state %>&bendy_banana_acceptance=yes">I accept</a> 
+    <a href="<%- data.callback %>#state=<%- data.state %>&bendy_banana_acceptance=no">I decline</a>
 </body>
 </html>
 */}
