11548a531d3b5c555b50eea1aaa8714e1d8161b1
Tomasz Janczuk <tomasz@janczuk.org>
2015-03-09 15:16:19 -0700

    udpate

--- webtask.js
+++ webtask.js
@@ -1,4 +1,20 @@
+var request = require('request');
+
 return function (context, callback) {
-    console.log('GitHub web hook fired!', context.webhook);
-    callback(null, 'Done');
-} 
+    if (context.data.slack_token && context.data.slack_channel) {
+        console.log('Posting message to slack for repository ' 
+            + context.webhook.repository.full_name);
+        var url = 'https://slack.com/api/chat.postMessage'
+            + '?token=' + context.data.slack_token
+            + '&channel=' + context.data.slack_channel
+            + '&user=' + (context.data.slack_user || 'WebTask')
+            + '&text=' + encodeURIComponent('Changes in `' + context.webhook.repository.full_name + '`');
+        request({ url: url, method: 'POST' }, function (error, res, body) {
+            callback(error, body);
+        });
+    }
+    else {
+        console.log('Repository ' + context.webhook.repository.full_name + ' but slack credentials not supplied.');
+        return callback();
+    }
+}
