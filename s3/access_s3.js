e3f84ccdb3c733528e8ca86f5a530fe5e06034e6
Tomasz Janczuk <tomasz@janczuk.org>
2015-03-09 11:39:03 -0700

    fix

--- s3/access_s3.js
+++ s3/access_s3.js
@@ -29,6 +29,8 @@ return function (context, req, res) {
         context.data.key = context.data.path.substring(i + 1);
     }
 
+    console.log('CONTEXT', context.data);
+
     // Authorize request
 
     var regexp = context.data[context.data.method + '_regex'];
