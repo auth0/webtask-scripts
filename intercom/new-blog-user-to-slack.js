7a45c6ad54d9772f79d28cf80ad9e142bcdcea8a
Martin Gontovnikas <martin@gon.to>
2015-07-27 21:44:07 -0700

    Added script

---
+++ intercom/new-blog-user-to-slack.js
@@ -0,0 +1,21 @@
+var request = require('request');
+
+return function (context, callback) {
+    
+    console.log(context.body);
+    // https://hooks.slack.com/services/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
+  //   request({
+  //       url: "https://slack.com/api/chat.postMessage",
+  //       method: 'POST',
+  //       json: true,
+  //       body: {
+  //         token: context.data.slack_token,  // Passed using secret params
+  //         channel: context.data.slack_channel,
+  //         user: context.data.slack_user || 'Webtask',
+  //         text: 'Push in `' + repo.full_name + '`'
+  //       }
+  //   },
+  //   function (error, res, body) {
+  //     callback(error, body);
+  // });
+};
