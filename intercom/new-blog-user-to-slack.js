a3f305ac33dbc093b16eb93068841e95d9389cfe
Martin Gontovnikas <martin@gon.to>
2015-07-27 22:13:33 -0700

    Updated post

--- intercom/new-blog-user-to-slack.js
+++ intercom/new-blog-user-to-slack.js
@@ -5,13 +5,13 @@ return function (context, callback) {
         context.body.data.item && 
         context.body.data.item.custom_attributes && 
         context.body.data.item.custom_attributes.blog_subs) {
-      
+
       request({
-        url: "https://hooks.slack.com/services/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",
+        url: context.data.SLACK_URL,
         method: 'POST',
         json: true,
         body: {
-          "text": "A new user has subscribed to the blog " + context.body.data.item.email
+          "text": context.data.SLACK_MESSAGE + context.body.data.item.email
         }
       },
       function (error, res, body) {
