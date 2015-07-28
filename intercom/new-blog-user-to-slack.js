d7901a55de46805c4d7e942e1fb9382c49970558
Martin Gontovnikas <martin@gon.to>
2015-07-27 21:52:09 -0700

    Webtask integration

--- intercom/new-blog-user-to-slack.js
+++ intercom/new-blog-user-to-slack.js
@@ -1,21 +1,22 @@
 var request = require('request');
 
 return function (context, callback) {
-    
-    console.log(context.body.data.item.custom_attributes);
-    // https://hooks.slack.com/services/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
-  //   request({
-  //       url: "https://slack.com/api/chat.postMessage",
-  //       method: 'POST',
-  //       json: true,
-  //       body: {
-  //         token: context.data.slack_token,  // Passed using secret params
-  //         channel: context.data.slack_channel,
-  //         user: context.data.slack_user || 'Webtask',
-  //         text: 'Push in `' + repo.full_name + '`'
-  //       }
-  //   },
-  //   function (error, res, body) {
-  //     callback(error, body);
-  // });
+    if (context.body.data && 
+        context.body.data.item && 
+        context.body.data.item.custom_attributes && 
+        context.body.data.item.custom_attributes.blog_subs) {
+      request({
+        url: "https://hooks.slack.com/services/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",
+        method: 'POST',
+        json: true,
+        body: {
+          "text": "A new user has subscribed to the blog " + context.body.data.item..email
+        }
+      },
+      function (error, res, body) {
+        return callback(error, body);
+      });
+    } else {
+      return callback(null, "Nothing to do");
+    }
 };
